28a4ca1d1e25775e7b25d37b3acca157a84e066d
Ron Mapue <ronmapue@gmail.com>
2020-10-25 15:40:05 -0700

    authentication

--- graphql-social-media/index.js
+++ graphql-social-media/index.js
@@ -1,23 +1,20 @@
 const { ApolloServer } = require('apollo-server');
-const gql = require('graphql-tag');
+const mongoose = require('mongoose');
 
-const typeDefs = gql`
-	type Query {
-		sayHi: String!
-	}
-`;
-
-const resolvers = {
-	Query: {
-		sayHi: () => 'Hello World',
-	},
-};
+const typeDefs = require('./graphql/typeDefs');
+const resolvers = require('./graphql/resolvers');
+const { MONGODB } = require('./config');
 
 const server = new ApolloServer({
 	typeDefs,
 	resolvers,
 });
 
-server.listen({ port: 5000 }).then((res) => {
-	console.log(`server running at ${res.url}`);
+mongoose.connect(MONGODB, { useNewUrlParser: true }).then(() => {
+	console.log('mongodb connected');
+	return server.listen({ port: 5000 });
 });
+
+// server.listen({ port: 5000 }).then((res) => {
+// 	console.log(`server running at ${res.url}`);
+// });
