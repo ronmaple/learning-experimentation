58a692991f746582c56f833438aee8f7655fe583
Ron Mapue <ronmapue@gmail.com>
2020-10-26 11:07:45 -0700

    backend done

--- graphql-social-media/graphql/resolvers/index.js
+++ graphql-social-media/graphql/resolvers/index.js
@@ -1,11 +1,21 @@
 const postsResolvers = require('./posts');
 const userResolvers = require('./users');
+const commentsResolver = require('./comments');
 
 module.exports = {
+	Post: {
+		likeCount: (parent) => parent.likes.length,
+		commentCount: (parent) => parent.comments.length,
+	},
 	Query: {
 		...postsResolvers.Query,
 	},
 	Mutation: {
 		...userResolvers.Mutation,
+		...postsResolvers.Mutation,
+		...commentsResolver.Mutation,
+	},
+	Subscription: {
+		...postsResolvers.Subscription,
 	},
 };
